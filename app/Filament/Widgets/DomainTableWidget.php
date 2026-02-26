cb2b25f56f81fcfed11a9f0d288fe1d16d0dfe5b
jstalinko <alinkodotid@gmail.com>
2026-02-26 12:38:31 +0700

    add tutorial flow

--- app/Filament/Widgets/DomainTableWidget.php
+++ app/Filament/Widgets/DomainTableWidget.php
@@ -18,37 +18,37 @@ class DomainTableWidget extends BaseWidget
     protected static ?string $heading = null;
 
 
-protected function setHeading(): void
-{
-    $domainUsed = Domain::where('user_id',auth()->user()->id)->count();
-    static::$heading = 'Registered : '.$domainUsed.' Domain |  Quota: ' . auth()->user()->domain_quota. ' Domain';
-}
+    protected function setHeading(): void
+    {
+        $domainUsed = Domain::where('user_id', auth()->user()->id)->count();
+        static::$heading = 'Registered : ' . $domainUsed . ' Domain |  Quota: ' . auth()->user()->domain_quota . ' Domain';
+    }
 
-public function mount(): void
-{
-    $this->setHeading();
-}
+    public function mount(): void
+    {
+        $this->setHeading();
+    }
     public function table(Table $table): Table
     {
         return $table
             ->query(
-                Domain::query()->where('user_id',auth()->user()->id)
+                Domain::query()->where('user_id', auth()->user()->id)
             )
             ->columns([
                 Tables\Columns\TextColumn::make('user.email')->label('Email Registered'),
                 Tables\Columns\TextColumn::make('signature'),
                 Tables\Columns\TextColumn::make('domain')
-                ->searchable()->badge(),
-            Tables\Columns\TextColumn::make('connection_type')->label('Connection Type')->badge(),
-            Tables\Columns\TextColumn::make('traffic_count')->label('Traffic Today')->formatStateUsing(fn($state) => Number::forHumans($state))
-                ->searchable(),
-            Tables\Columns\TextColumn::make('created_at')
-                ->dateTime()
-                ->sortable()
-              
+                    ->searchable()->badge(),
+                Tables\Columns\TextColumn::make('connection_type')->label('Connection Type')->badge(),
+                Tables\Columns\TextColumn::make('traffic_count')->label('Traffic Today')->formatStateUsing(fn($state) => Number::forHumans($state))
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+
             ])->headerActions([
-               // Action::make('join')->label('JOIN TELEGRAM GROUP MEMBER')->action(fn() => redirect()->away('https://t.me/+a6MGYNGFGeVkYWI1')),
-               // Action::make('docs')->label('DOCUMENTATION READ HERE')->action(fn() => redirect()->away('/docs'))->color('success'),
+                Action::make('tutorial_flow')->label('TUTORIAL FLOW ')->action(fn() => redirect()->away('https://www.youtube.com/watch?v=dJmfIJGPCMk'))->color('danger')->icon('heroicon-o-play'),
+
             ]);
     }
 }
